05f952a9850cdc7083affdfdae704df11dab26e7
rifmeister <cherifyounoussn@gmail.com>
2018-07-13 16:49:41 +0000

    Model du client

---
+++ app/Client.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+//Trait pour envoyer des notifications
+use Illuminate\Notifications\Notifiable;
+//Notifications pour le client
+use App\Notifications\ClientResetPasswordNotification;
+
+class Client extends Authenticatable
+{
+    use Notifiable;
+
+ //Mass assignable attributes
+  protected $fillable = [
+      'nom', 'prenom', 'num_tel', 'email', 'password',
+  ];
+
+  //hidden attributes
+   protected $hidden = [
+       'password', 'remember_token',
+   ];    
+
+   public function sendPasswordResetNotification($token){
+       $this->notify(new ClientResetPasswordNotification($token));
+   }
+}
