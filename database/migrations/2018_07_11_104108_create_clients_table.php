ab1468718668ec070d00ad70f80f6ebad3d97355
rifmeister <cherifyounoussn@gmail.com>
2018-07-16 11:07:50 +0000

    Migration : table client

---
+++ database/migrations/2018_07_11_104108_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom',50);
+            $table->string('prenom',50);
+            $table->string('email')->unique();
+            $table->integer('num_tel')->unique();
+            $table->string('password');
+            $table->integer('cni');
+            $table->date('date_naissance');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
